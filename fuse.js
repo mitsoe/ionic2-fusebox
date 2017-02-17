4806b6fe3aead11b4f02579c2e050c1bfb0da95e
mitch lamers <mitchlamers@MacBook-Pro-van-mitch.local>
2017-02-17 22:37:35 +0100

    Can now run with fuse, icons not working as of now

--- fuse.js
+++ fuse.js
@@ -1,22 +1,22 @@
-//Not used, check gulpfile.js for configuration
 const fsbx = require('fuse-box');
 
-const DIST = 'www';
-
 const fuseBox = fsbx.FuseBox.init({
+    cache: false,
     homeDir: 'src/',
     sourceMap: {
         bundleReference: 'app.js.map',
-        outFile: `./${DIST}/bundle/app.js.map`,
+        outFile: './www/app.js.map',
     },
-    outFile: `./${DIST}/bundle/app.js`,
+    outFile: './www/app.js',
     plugins: [
+        fsbx.CSSPlugin({ write: true }),
+
         [
             fsbx.SassPlugin({ outputStyle: 'compressed' }),
-            fsbx.CSSResourcePlugin({
-                inline: true
-            }), fsbx.CSSPlugin()
+            fsbx.CSSPlugin()
         ],
+
+        fsbx.TypeScriptHelpers(),
         fsbx.JSONPlugin(),
         fsbx.HTMLPlugin({ useDefault: false })
     ]
