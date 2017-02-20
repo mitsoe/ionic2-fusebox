01fb025508aa90d15a2716fd677e535ec466d82e
mitch lamers <mitchlamers@MacBook-Pro-van-mitch.local>
2017-02-20 19:39:24 +0100

    Use prestart to copy index.html

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,6 @@ const rename = require('gulp-rename');
 const fsbx = require('fuse-box');
 const DIST = 'www';
 
-gulp.task('index', () => {
-    return gulp.src('src/index.html')
-        .pipe(gulp.dest(DIST));
-});
-
 gulp.task('ionic-fonts', () => {
     return gulp.src('node_modules/ionic-angular/fonts/*')
         .pipe(rename({
@@ -16,8 +11,8 @@ gulp.task('ionic-fonts', () => {
         .pipe(gulp.dest(DIST));
 });
 
-gulp.task('watch', ['index', 'ionic-fonts'], () => {
-    gulp.watch('src/**/*.**', ['fusebox', 'index']);
+gulp.task('watch', ['ionic-fonts'], () => {
+    gulp.watch('src/**/*.**', ['fusebox']);
 });
 
 gulp.task('default', ['watch'], () => {
