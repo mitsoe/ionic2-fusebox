5a9f9051839af5cd486443f3984ee5e4f394f29a
Nova Framework <mitch.lamers@planonsoftware.com>
2017-02-20 16:37:06 +0100

    Add storage and cause import token error

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { Platform } from 'ionic-angular';
 
 @Component({
     selector: 'page-home',
     template: require('./home.html')
 })
 export class HomePage {
+    public storage: Storage
+    constructor(private platform: Platform) {
+        this.platform.ready().then(() => {
+            if (this.storage == undefined) {
+                this.storage = new Storage();
+            }
+        });
+    }
 
 }
