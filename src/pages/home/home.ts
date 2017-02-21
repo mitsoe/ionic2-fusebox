49571f982fa9dcf85e6ad6f1c777f347c564d37c
Nova Framework <mitch.lamers@planonsoftware.com>
2017-02-21 16:05:23 +0100

    home.ts includes storage; breaks app.js

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
     selector: 'page-home',
     template: require('./home.html')
 })
 export class HomePage {
-
+    public store: Storage;
+    constructor(public storage: Storage) {
+        this.store = new Storage();
+    }
 }
