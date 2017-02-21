4a827dc8d34e55e536d7be59311c7a9d69cbc621
burak-sinikci <burak.sinikci@cbr.de>
2017-02-21 08:39:50 +0100

    Update Gulp Task

--- fuse.js
+++ fuse.js
@@ -1,27 +1,27 @@
 const fsbx = require('fuse-box');
 
 const fuseBox = fsbx.FuseBox.init({
-    cache: false,
-    homeDir: 'src/',
-    sourceMap: {
-        bundleReference: 'app.js.map',
-        outFile: './www/app.js.map',
-    },
-    outFile: './www/app.js',
-    plugins: [
-        fsbx.CSSPlugin({ write: true }),
+  cache    : false,
+  homeDir  : 'src/',
+  sourceMap: {
+    bundleReference: 'app.js.map',
+    outFile        : './www/app.js.map'
+  },
+  outFile  : './www/app.js',
+  plugins  : [
+    fsbx.CSSPlugin({write: true}),
 
-        [
-            fsbx.SassPlugin({ outputStyle: 'compressed' }),
-            fsbx.CSSPlugin()
-        ],
+    [
+      fsbx.SassPlugin({outputStyle: 'compressed'}),
+      fsbx.CSSPlugin()
+    ],
 
-        fsbx.TypeScriptHelpers(),
-        fsbx.JSONPlugin(),
-        fsbx.HTMLPlugin({ useDefault: false })
-    ]
+    fsbx.TypeScriptHelpers(),
+    fsbx.JSONPlugin(),
+    fsbx.HTMLPlugin({useDefault: false})
+  ]
 });
 
 fuseBox.devServer('>main.ts', {
-    port: 8100
+  port: 8100
 });
--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,11 @@ const rename = require('gulp-rename');
 const fsbx = require('fuse-box');
 const DIST = 'www';
 
-gulp.task('ionic-fonts', () => {
-    return gulp.src('node_modules/ionic-angular/fonts/*')
-        .pipe(rename({
-            dirname: './fonts'
-        }))
-        .pipe(gulp.dest(DIST));
-});
 
-gulp.task('watch', ['ionic-fonts'], () => {
-    gulp.watch('src/**/*.**', ['fusebox']);
-});
-
-gulp.task('default', ['watch'], () => {
-    gulp.watch(`${DIST}/**/*`);
+gulp.task('ionic-fonts', function () {
+  return gulp.src('node_modules/ionic-angular/fonts/*')
+    .pipe(rename({
+      dirname: './fonts'
+    }))
+    .pipe(gulp.dest(DIST))
 });
