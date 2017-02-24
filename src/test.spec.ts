9c6191a82a24b90b22dd720532ffe26cf28a75b3
Nova Framework <mitch.lamers@planonsoftware.com>
2017-02-24 14:30:21 +0100

    Add test

---
+++ src/test.spec.ts
@@ -0,0 +1,21 @@
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { expect } from 'chai';
+
+import {
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting
+}
+    from '@angular/platform-browser-dynamic/testing';
+
+
+describe('test', () => {
+
+    it('calls service to get todos', () => {
+        // expect(true).to.eq(true)
+        expect(true).to.eq(true);
+    });
+
+});
